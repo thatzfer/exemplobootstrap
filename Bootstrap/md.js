2927a94f4b4320a57510345983a188608c2e28f4
Thaís Ferraz <72307122+thatzfer@users.noreply.github.com>
2021-08-09 21:47:54 -0300

    Add files via upload

---
+++ Bootstrap/md.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {});
+  });
